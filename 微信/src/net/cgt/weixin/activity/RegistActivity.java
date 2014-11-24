725974d307768c75da468e43563757ba5cd47f22
lijian17 <374452668@qq.com>
2014-11-24 18:07:10 +0800

    注册

---
+++ 微信/src/net/cgt/weixin/activity/RegistActivity.java
@@ -0,0 +1,20 @@
+package net.cgt.weixin.activity;
+
+import net.cgt.weixin.R;
+import android.os.Bundle;
+
+/**
+ * 注册
+ * 
+ * @author lijian
+ * @date 2014-11-22
+ */
+public class RegistActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.cgt_activity_regist);
+	}
+
+}
