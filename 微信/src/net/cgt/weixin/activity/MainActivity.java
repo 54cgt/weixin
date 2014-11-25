f121e67e535c38fdaf1fcaf1f5cd82cef33b26b1
lijian17 <374452668@qq.com>
2014-11-25 23:30:16 +0800

    weixin->ActionBar

--- 微信/src/net/cgt/weixin/activity/MainActivity.java
+++ 微信/src/net/cgt/weixin/activity/MainActivity.java
@@ -1,21 +1,33 @@
 package net.cgt.weixin.activity;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import net.cgt.weixin.Constants;
 import net.cgt.weixin.GlobalParams;
 import net.cgt.weixin.R;
 import net.cgt.weixin.factory.FragmentFactory;
+import net.cgt.weixin.utils.AppToast;
 import net.cgt.weixin.utils.L;
+import net.cgt.weixin.utils.LogUtil;
 import net.cgt.weixin.utils.SpUtil;
 import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuItem.OnActionExpandListener;
+import android.view.ViewConfiguration;
+import android.view.Window;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.SearchView;
 
 /**
  * 主控制界面
@@ -26,7 +38,8 @@ import android.widget.RadioGroup.OnCheckedChangeListener;
 @SuppressLint("NewApi")
 public class MainActivity extends Activity implements OnCheckedChangeListener {
 
-	private static final String TAG = "MainActivity";
+	private static final String LOGTAG = LogUtil.makeLogTag(MainActivity.class);
+
 	private FragmentManager mFragmentManager;
 	private RadioGroup mRg_tab;
 	private SpUtil sp;
@@ -34,7 +47,7 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		if (!GlobalParams.ISLOGIN) {
-			L.d(TAG, "MainActivity--->isLogin");
+			L.d(LOGTAG, "MainActivity--->isLogin");
 			Intent intent = new Intent(this, LoginActivity.class);
 			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 			intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
@@ -60,6 +73,10 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 	}
 
 	private void initView() {
+		ActionBar actionBar = this.getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(false);
+		setOverflowShowingAlways();
+
 		mFragmentManager = getFragmentManager();
 		mRg_tab = (RadioGroup) findViewById(R.id.cgt_rg_tab);
 		mRg_tab.setOnCheckedChangeListener(this);
@@ -70,6 +87,20 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 		int id = sp.getInt(Constants.PAGENUMBER, 1);
 
 		showPreviousPage(id);
+	}
+
+	/**
+	 * 通过反射得到Android的有无物理Menu键
+	 */
+	private void setOverflowShowingAlways() {
+		try {
+			ViewConfiguration config = ViewConfiguration.get(this);
+			Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+			menuKeyField.setAccessible(true);
+			menuKeyField.setBoolean(config, false);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -136,9 +167,86 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 		transaction.commit();
 	}
 
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		getMenuInflater().inflate(R.menu.main, menu);
-//		return true;
-//	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.cgt_menu_weixin, menu);
+		MenuItem searchItem = menu.findItem(R.id.menu_weixin_search);
+		SearchView searchView = (SearchView) searchItem.getActionView();
+//		// 给SearchView添加展开/合并的监听
+//		searchItem.setOnActionExpandListener(new OnActionExpandListener() {
+//
+//			@Override
+//			public boolean onMenuItemActionExpand(MenuItem item) {
+//				L.i(LOGTAG, "on expand");
+//				AppToast.getToast().show("搜索展开");
+//				return false;
+//			}
+//
+//			@Override
+//			public boolean onMenuItemActionCollapse(MenuItem item) {
+//				L.i(LOGTAG, "on collapse");
+//				AppToast.getToast().show("搜索合并");
+//				return false;
+//			}
+//		});
+
+		return super.onCreateOptionsMenu(menu);//true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			AppToast.getToast().show("返回上一页");
+			// Intent upIntent = NavUtils.getParentActivityIntent(this);
+			// if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+			//
+			// TaskStackBuilder.create(this)
+			// .addNextIntentWithParentStack(upIntent).startActivities();
+			// } else {
+			// upIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			// NavUtils.navigateUpTo(this, upIntent);
+			// }
+
+			break;
+		case R.id.menu_weixin_groupChat:
+			AppToast.getToast().show(R.string.text_menu_weixin_groupChat);
+			break;
+		case R.id.menu_weixin_addFriend:
+			AppToast.getToast().show(R.string.text_menu_weixin_addFriend);
+			break;
+		case R.id.menu_weixin_sweep:
+			AppToast.getToast().show(R.string.text_menu_weixin_sweep);
+			break;
+		case R.id.menu_weixin_feedback:
+			AppToast.getToast().show(R.string.text_menu_weixin_feedback);
+			break;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * overflow被展开的时候调用
+	 * 
+	 * @param featureId
+	 * @param menu
+	 * @return
+	 */
+	@Override
+	public boolean onMenuOpened(int featureId, Menu menu) {
+		//通过返回反射的方法将MenuBuilder的setOptionalIconsVisible变量设置为true
+		if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+			if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+				try {
+					Method m = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+					m.setAccessible(true);
+					m.invoke(menu, true);
+				} catch (Exception e) {
+				}
+			}
+		}
+		return super.onMenuOpened(featureId, menu);
+	}
 }
--- 微信/src/net/cgt/weixin/fragment/WeixinFragment.java
+++ 微信/src/net/cgt/weixin/fragment/WeixinFragment.java
@@ -1,25 +1,17 @@
 package net.cgt.weixin.fragment;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 import net.cgt.weixin.GlobalParams;
 import net.cgt.weixin.R;
 import net.cgt.weixin.domain.User;
-import net.cgt.weixin.utils.AppToast;
 import net.cgt.weixin.view.adapter.RosterAdapter;
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.SearchView;
 
 /**
  * 微信
@@ -32,40 +24,21 @@ public class WeixinFragment extends BaseFragment {
 
 	private ListView mLv_weixin;
 	private RosterAdapter mRosterAdapter;
-	private View v;
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		v = inflater.inflate(R.layout.cgt_fragment_weixin, null);
-		init();
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.cgt_fragment_weixin, null);
+		init(v);
 		return v;
 	}
 
-	private void init() {
-		initView();
+	private void init(View v) {
+		initView(v);
 		initData();
 	}
 
-	private void initView() {
-//		ActionBar actionBar = GlobalParams.activity.getActionBar();
-//		actionBar.setDisplayHomeAsUpEnabled(true);
-//		setOverflowShowingAlways();
-
+	private void initView(View v) {
 		mLv_weixin = (ListView) v.findViewById(R.id.cgt_lv_weixin);
-	}
-
-	private void setOverflowShowingAlways() {
-		try {
-			ViewConfiguration config = ViewConfiguration
-					.get(GlobalParams.activity);
-			Field menuKeyField = ViewConfiguration.class
-					.getDeclaredField("sHasPermanentMenuKey");
-			menuKeyField.setAccessible(true);
-			menuKeyField.setBoolean(config, false);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 
 	private void initData() {
@@ -79,54 +52,10 @@ public class WeixinFragment extends BaseFragment {
 		}
 
 		if (mRosterAdapter == null) {
-			mRosterAdapter = new RosterAdapter(GlobalParams.activity,
-					mList_user);
+			mRosterAdapter = new RosterAdapter(GlobalParams.activity, mList_user);
 			mLv_weixin.setAdapter(mRosterAdapter);
 		} else {
 			mRosterAdapter.notifyDataSetChanged();
 		}
 	}
-
-	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		super.onCreateOptionsMenu(menu, inflater);
-		inflater.inflate(R.menu.cgt_menu_weixin, menu);
-		MenuItem searchItem = menu.findItem(R.id.menu_weixin_search);
-		SearchView searchView = (SearchView) searchItem.getActionView();
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			AppToast.getToast().show("返回上一页");
-			// Intent upIntent = NavUtils.getParentActivityIntent(this);
-			// if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
-			//
-			// TaskStackBuilder.create(this)
-			// .addNextIntentWithParentStack(upIntent).startActivities();
-			// } else {
-			// upIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			// NavUtils.navigateUpTo(this, upIntent);
-			// }
-
-			break;
-		case R.id.menu_weixin_groupChat:
-			AppToast.getToast().show(R.string.text_menu_weixin_groupChat);
-			break;
-		case R.id.menu_weixin_addFriend:
-			AppToast.getToast().show(R.string.text_menu_weixin_addFriend);
-			break;
-		case R.id.menu_weixin_sweep:
-			AppToast.getToast().show(R.string.text_menu_weixin_sweep);
-			break;
-		case R.id.menu_weixin_feedback:
-			AppToast.getToast().show(R.string.text_menu_weixin_feedback);
-			break;
-		default:
-			break;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
 }
