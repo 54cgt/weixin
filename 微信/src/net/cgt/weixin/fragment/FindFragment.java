08c6855eb4b3e68101ea60b79885b849b548aad9
lijian17 <374452668@qq.com>
2014-11-26 23:39:49 +0800

    各主界面UI初步

--- 微信/src/net/cgt/weixin/fragment/FindFragment.java
+++ 微信/src/net/cgt/weixin/fragment/FindFragment.java
@@ -1,11 +1,94 @@
 package net.cgt.weixin.fragment;
 
+import net.cgt.weixin.R;
+import net.cgt.weixin.utils.AppToast;
+import net.cgt.weixin.utils.L;
+import net.cgt.weixin.utils.LogUtil;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
 /**
  * 发现
  * 
- * @author lijian-pc
- * 
+ * @author lijian
+ * @date 2014-11-26
  */
-public class FindFragment extends BaseFragment {
+public class FindFragment extends BaseFragment implements OnClickListener {
 
+	private static final String LOGTAG = LogUtil.makeLogTag(FindFragment.class);
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.cgt_fragment_find, null);
+		init(v);
+		return v;
+	}
+
+	private void init(View v) {
+		initView(v);
+		initData();
+	}
+
+	private void initView(View v) {
+		LinearLayout mLl_friends = (LinearLayout) v.findViewById(R.id.cgt_ll_find_friends);
+		LinearLayout mLl_sweep = (LinearLayout) v.findViewById(R.id.cgt_ll_find_sweep);
+		LinearLayout mLl_shake = (LinearLayout) v.findViewById(R.id.cgt_ll_find_shake);
+		LinearLayout mLl_nearbyPeople = (LinearLayout) v.findViewById(R.id.cgt_ll_find_nearbyPeople);
+		LinearLayout mLl_driftBottle = (LinearLayout) v.findViewById(R.id.cgt_ll_find_driftBottle);
+		LinearLayout mLl_shopping = (LinearLayout) v.findViewById(R.id.cgt_ll_find_shopping);
+		LinearLayout mLl_game = (LinearLayout) v.findViewById(R.id.cgt_ll_find_game);
+		mLl_friends.setOnClickListener(this);
+		mLl_sweep.setOnClickListener(this);
+		mLl_shake.setOnClickListener(this);
+		mLl_nearbyPeople.setOnClickListener(this);
+		mLl_driftBottle.setOnClickListener(this);
+		mLl_shopping.setOnClickListener(this);
+		mLl_game.setOnClickListener(this);
+	}
+
+	private void initData() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.cgt_ll_find_friends:
+			AppToast.getToast().show(R.string.text_find_friends);
+			L.i(LOGTAG, "朋友圈");
+			break;
+		case R.id.cgt_ll_find_sweep:
+			AppToast.getToast().show(R.string.text_find_sweep);
+			L.i(LOGTAG, "扫一扫");
+			break;
+		case R.id.cgt_ll_find_shake:
+			AppToast.getToast().show(R.string.text_find_shake);
+			L.i(LOGTAG, "摇一摇");
+			break;
+		case R.id.cgt_ll_find_nearbyPeople:
+			AppToast.getToast().show(R.string.text_find_nearbyPeople);
+			L.i(LOGTAG, "附近的人");
+			break;
+		case R.id.cgt_ll_find_driftBottle:
+			AppToast.getToast().show(R.string.text_find_driftBottle);
+			L.i(LOGTAG, "漂流瓶");
+			break;
+		case R.id.cgt_ll_find_shopping:
+			AppToast.getToast().show(R.string.text_find_shopping);
+			L.i(LOGTAG, "购物");
+			break;
+		case R.id.cgt_ll_find_game:
+			AppToast.getToast().show(R.string.text_find_game);
+			L.i(LOGTAG, "游戏");
+			break;
+
+		default:
+			break;
+		}
+	}
 }
--- 微信/src/net/cgt/weixin/fragment/MeFragment.java
+++ 微信/src/net/cgt/weixin/fragment/MeFragment.java
@@ -1,11 +1,100 @@
 package net.cgt.weixin.fragment;
 
+import net.cgt.weixin.R;
+import net.cgt.weixin.utils.AppToast;
+import net.cgt.weixin.utils.L;
+import net.cgt.weixin.utils.LogUtil;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 /**
  * 我页面
  * 
- * @author lijian-pc
- * 
+ * @author lijian
+ * @date 2014-11-26
  */
-public class MeFragment extends BaseFragment {
+public class MeFragment extends BaseFragment implements OnClickListener {
 
+	private static final String LOGTAG = LogUtil.makeLogTag(MeFragment.class);
+	private ImageView mIv_userPic;
+	private TextView mTv_userName;
+	private TextView mTv_weixinNum;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.cgt_fragment_me, null);
+		init(v);
+		return v;
+	}
+
+	private void init(View v) {
+		initView(v);
+		initData();
+	}
+
+	private void initView(View v) {
+		LinearLayout mLl_userInfo = (LinearLayout) v.findViewById(R.id.cgt_ll_me_userInfo);
+		mIv_userPic = (ImageView) v.findViewById(R.id.cgt_iv_me_userPic);
+		mTv_userName = (TextView) v.findViewById(R.id.cgt_tv_me_userName);
+		mTv_weixinNum = (TextView) v.findViewById(R.id.cgt_tv_me_weixinNum);
+		ImageButton mIb_qrCOde = (ImageButton) v.findViewById(R.id.cgt_ib_me_qrCode);
+
+		LinearLayout mLl_photo = (LinearLayout) v.findViewById(R.id.cgt_ll_me_photo);
+		LinearLayout mLl_collect = (LinearLayout) v.findViewById(R.id.cgt_ll_me_collect);
+		LinearLayout mLl_wallet = (LinearLayout) v.findViewById(R.id.cgt_ll_me_wallet);
+		LinearLayout mLl_set = (LinearLayout) v.findViewById(R.id.cgt_ll_me_set);
+
+		mLl_userInfo.setOnClickListener(this);
+		mIb_qrCOde.setOnClickListener(this);
+		mLl_photo.setOnClickListener(this);
+		mLl_collect.setOnClickListener(this);
+		mLl_wallet.setOnClickListener(this);
+		mLl_set.setOnClickListener(this);
+	}
+
+	private void initData() {
+		mIv_userPic.setImageResource(R.drawable.user_picture);
+		mTv_userName.setText("深情小建");
+		mTv_weixinNum.setText("lijian374452668");
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.cgt_ll_me_userInfo:
+			AppToast.getToast().show("用户信息");
+			L.i(LOGTAG, "用户信息");
+			break;
+		case R.id.cgt_ib_me_qrCode:
+			AppToast.getToast().show("二维码");
+			L.i(LOGTAG, "二维码");
+			break;
+		case R.id.cgt_ll_me_photo:
+			AppToast.getToast().show(R.string.text_me_photo);
+			L.i(LOGTAG, "相册");
+			break;
+		case R.id.cgt_ll_me_collect:
+			AppToast.getToast().show(R.string.text_me_collect);
+			L.i(LOGTAG, "收藏");
+			break;
+		case R.id.cgt_ll_me_wallet:
+			AppToast.getToast().show(R.string.text_me_wallet);
+			L.i(LOGTAG, "钱包");
+			break;
+		case R.id.cgt_ll_me_set:
+			AppToast.getToast().show(R.string.text_me_set);
+			L.i(LOGTAG, "设置");
+			break;
+
+		default:
+			break;
+		}
+	}
 }
