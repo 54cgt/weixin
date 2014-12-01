378ce9597227842e07225244a60ca52375cb8786
lijian17 <374452668@qq.com>
2014-12-01 23:37:18 +0800

    update AppDialog

---
+++ 微信/src/net/cgt/weixin/utils/AppDialog.java
@@ -0,0 +1,33 @@
+package net.cgt.weixin.utils;
+
+import net.cgt.weixin.R;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+
+public class AppDialog extends Dialog {
+
+	private Context mContext;
+
+	protected AppDialog(Context context, boolean cancelable, OnCancelListener cancelListener) {
+		super(context, cancelable, cancelListener);
+		this.mContext = context;
+	}
+
+	public AppDialog(Context context, int theme) {
+		super(context, theme);
+		this.mContext = context;
+	}
+
+	public AppDialog(Context context) {
+		super(context);
+		this.mContext = context;
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dialog);
+	}
+
+}
